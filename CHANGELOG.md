986ebdd6d297c38f676aee05bb7bad9e119e7971
Douglas Pouk <doug@flyonlinetools.com>
2017-12-13 09:53:11 -0600

    2.0.2 chrome headless

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,6 @@
 
 ## 2.0.1
 Removed trigger('click') to support selenium.
+
+## 2.0.2
+Improved logic for selenium_chrome_headless.
--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -8,31 +8,39 @@ module Capybara
     ##
     def select2(value, **options)
       from = options[:from]
+      search_input = from + "-search__field"
       raise "You must specify a CSS ID selector." unless from.present? and from[0] == "#"
       execute_script %| $('#{from}').select2('open'); |
+sleep(0.1)
       search = options[:search]
       #first(".select2-search__field").set(search)
       if search.present?
-        execute_script %|$("input.select2-search__field:visible").val("#{search}");|
-        execute_script %|$("input.select2-search__field:visible").trigger("input");|
-        sleep 1
+        # type the search term into the search input field
+#        fill_in search_input, :with=>search
+        execute_script %|$('#{search_input}').val('#{j search}');|
+        execute_script %|$('#{search_input}').trigger('input');|
       end
+sleep 0.5
 
       # the results are in a UL with the id="select2-booking_client_id-results"
       results_container = "#select2-%s-results" % from.gsub('#','')
 
       # this will cause Capybara to wait for the ajax results
-      wait_until{ find(results_container).visible? }
+      # wait_until{ find(results_container).first('li:not(.loading-results)').present? }
+      expect(page).to have_css 'li.select2-results__option:not(.loading-results)'
+
       # check for a nested/grouped value
       # select the last element, because grouped results are nested within an outer li and we want the inner one
       e = all("li", text: /#{Regexp.escape(value)}/).last
       if e.present?
         # click it
+        e.hover
         e.click
       else
         raise "%s :value not found" % value
       end
-      execute_script %| $('#{from}').select2('close'); |
+      #execute_script %| $('#{from}').select2('close'); |
+sleep 0.2
     end
   end
 end
